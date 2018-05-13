707e57fb94756ef61f6f2d62f74a8181a4374860
Leonardo Fontana <tetsuo2501@gmail.com>
2018-05-13 17:11:18 +0200

    added feature to behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,12 +3,19 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+use SpreadsheetExcelReader\Reader;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    /**
+     * @var Spreadsheet_Excel_Reader
+     */
+    protected $excel;
+    
     /**
      * Initializes context.
      *
@@ -18,5 +25,22 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+    
+    /**
+     * @Given document called :fileName
+     */
+    public function ilDocumentoChiamato($fileName)
+    {
+        $this->excel = new Reader();
+        $this->excel->read(__DIR__ . '/../Fixtures/' . $fileName);
+    }
+
+    /**
+     * @Then va bene così
+     */
+    public function vaBeneCosi()
+    {
+        
     }
 }
---
+++ features/openFile.feature
@@ -0,0 +1,4 @@
+Feature: Parsing del file
+Scenario: Open a existent file
+     Given document called "sample.xls"
+      Then va bene così
