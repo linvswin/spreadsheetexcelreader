cfd292c6e9da836b88b9fccae9219c30e7e224ca
Leonardo Fontana <tetsuo2501@gmail.com>
2018-05-27 10:31:50 +0200

    added tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,7 +4,8 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\Behat\Tester\Exception\PendingException;
-use SpreadsheetExcelReader\Reader;
+use Spreadsheet\Excel\Reader;
+use PHPUnit\Framework\Assert;
 
 /**
  * Defines application features from the specific context.
@@ -12,9 +13,14 @@ use SpreadsheetExcelReader\Reader;
 class FeatureContext implements Context
 {
     /**
-     * @var Spreadsheet_Excel_Reader
+     * @var Reader
      */
     protected $excel;
+
+    /**
+     * @var int
+     */
+    protected $sheet = 0;
     
     /**
      * Initializes context.
@@ -30,17 +36,43 @@ class FeatureContext implements Context
     /**
      * @Given document called :fileName
      */
-    public function ilDocumentoChiamato($fileName)
+    public function givenDocument($fileName)
     {
         $this->excel = new Reader();
         $this->excel->read(__DIR__ . '/../Fixtures/' . $fileName);
     }
 
     /**
-     * @Then va bene così
+     * @Given sheet :index
      */
-    public function vaBeneCosi()
+    public function givenSheet($index){
+        $this->sheet = $index;
+    }
+
+    /**
+     * @Then the row :row and the column :col contains :expected
+     */
+    public function cellContains($row, $col, $expected)
     {
-        
+        $cells = $this->excel->sheets[$this->sheet]['cells'];
+        Assert::arrayHasKey($row)->evaluate($cells, "the sheet doesn't contains row $row");
+        $rowValue = $cells[$row];
+        Assert::arrayHasKey($col)->evaluate($rowValue, "The row doesn't contain column $col");
+        $value = $rowValue[$col];
+        Assert::assertEquals($expected, $value);
+    }
+
+    /**
+     * @Then the row :row and the column :col is empty
+     */
+    public function cellEmpty($row, $col){
+        Assert::assertArrayNotHasKey($col, $this->excel->sheets[$this->sheet]['cells'][$row]);
+    }
+
+    /**
+     *@Then the sheet exists 
+    */
+    public function sheetExists(){
+        Assert::assertArrayHasKey($this->sheet, $this->excel->sheets);
     }
 }
--- features/openFile.feature
+++ features/openFile.feature
@@ -1,4 +1,8 @@
 Feature: Parsing del file
 Scenario: Open a existent file
      Given document called "sample.xls"
-      Then va bene così
+       And sheet 0
+      Then The sheet exists     
+       And the row 1 and the column 1 contains 'type'
+       And the row 73 and the column 5 contains 1234567890.1234567890
+       And the row 2 and the column 3 is empty
